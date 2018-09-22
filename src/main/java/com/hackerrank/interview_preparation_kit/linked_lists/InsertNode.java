5a44336359476d23f6ce13b590e367923a1a5113
Hau Le <leconghau.me@gmail.com>
2018-09-23 01:06:04 +0700

    https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list

--- src/main/java/com/hackerrank/interview_preparation_kit/linked_lists/InsertNode.java
+++ src/main/java/com/hackerrank/interview_preparation_kit/linked_lists/InsertNode.java
@@ -1,4 +1,84 @@
 package com.hackerrank.interview_preparation_kit.linked_lists;
 
+import java.util.Scanner;
+
 public class InsertNode {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            llist.insertNode(llistItem);
+        }
+
+        int data = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int position = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        SinglyLinkedListNode llist_head = insertNodeAtPosition(llist.head, data, position);
+
+        printSinglyLinkedList(llist_head, "->");
+
+        scanner.close();
+    }
+
+    private static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) {
+        do {
+            System.out.print(String.valueOf(node.data) + sep);
+            node = node.next;
+        } while (node != null);
+    }
+
+    private static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+        SinglyLinkedListNode tmp = head;
+        for (int i = 0; i < position - 1; i++) {
+            tmp = tmp.next;
+        }
+        SinglyLinkedListNode node = new SinglyLinkedListNode(data);
+        SinglyLinkedListNode tmp2 = tmp.next;
+        tmp.next = node;
+        node.next = tmp2;
+        return head;
+    }
+}
+
+class SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode next;
+
+    SinglyLinkedListNode(int nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    SinglyLinkedListNode head;
+    private SinglyLinkedListNode tail;
+
+    SinglyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    void insertNode(int nodeData) {
+        SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
 }
