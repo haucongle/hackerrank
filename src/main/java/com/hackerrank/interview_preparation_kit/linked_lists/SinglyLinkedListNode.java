dbfa2e38fa9cf30710a4844a6826c98967ca9613
Hau Le <leconghau.me@gmail.com>
2018-09-23 03:45:12 +0700

    minor

--- src/main/java/com/hackerrank/interview_preparation_kit/linked_lists/SinglyLinkedListNode.java
+++ src/main/java/com/hackerrank/interview_preparation_kit/linked_lists/SinglyLinkedListNode.java
@@ -1,10 +1,10 @@
 package com.hackerrank.interview_preparation_kit.linked_lists;
 
 class SinglyLinkedListNode {
-    public int data;
-    public SinglyLinkedListNode next;
+    int data;
+    SinglyLinkedListNode next;
 
-    public SinglyLinkedListNode(int nodeData) {
+    SinglyLinkedListNode(int nodeData) {
         this.data = nodeData;
         this.next = null;
     }
