dc6d08f714da094fff17feab65f11eb418eb0be1
Hau Le <leconghau.me@gmail.com>
2018-09-23 14:09:24 +0700

    https://www.hackerrank.com/challenges/find-the-merge-point-of-two-joined-linked-lists

---
+++ src/main/java/com/hackerrank/interview_preparation_kit/linked_lists/MergeNode.java
@@ -0,0 +1,80 @@
+package com.hackerrank.interview_preparation_kit.linked_lists;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class MergeNode {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        SinglyLinkedListNode currentA = head1;
+        SinglyLinkedListNode currentB = head2;
+
+        while (currentA != currentB) {
+            if (currentA.next == null) {
+                currentA = head2;
+            } else {
+                currentA = currentA.next;
+            }
+            currentB = Objects.requireNonNullElse(currentB.next, head1);
+        }
+        return currentB.data;
+
+    }
+
+    public static void main(String[] args) {
+        int tests = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            int index = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            SinglyLinkedList llist1 = new SinglyLinkedList();
+
+            int llist1Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llist1Count; i++) {
+                int llist1Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist1.insertNode(llist1Item);
+            }
+
+            SinglyLinkedList llist2 = new SinglyLinkedList();
+
+            int llist2Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llist2Count; i++) {
+                int llist2Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist2.insertNode(llist2Item);
+            }
+
+            SinglyLinkedListNode ptr1 = llist1.head;
+            SinglyLinkedListNode ptr2 = llist2.head;
+
+            for (int i = 0; i < llist1Count; i++) {
+                if (i < index) {
+                    ptr1 = ptr1.next;
+                }
+            }
+
+            for (int i = 0; i < llist2Count; i++) {
+                if (i != llist2Count - 1) {
+                    ptr2 = ptr2.next;
+                }
+            }
+
+            ptr2.next = ptr1;
+
+            int result = findMergeNode(llist1.head, llist2.head);
+            System.out.println(result);
+        }
+
+        scanner.close();
+    }
+}
